f904e105a0e910d0fe3933dbf81082cf00325c0d
re150 <a21110175@ceti.mx>
2023-05-08 20:11:47 -0600

    actulizar

--- app/src/main/java/com/example/fragmentos/DataFragment.kt
+++ app/src/main/java/com/example/fragmentos/DataFragment.kt
@@ -1,7 +1,9 @@
 package com.example.fragmentos
 
 
+import android.content.Context
 import android.os.Bundle
+import android.os.ProxyFileDescriptorCallback
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -14,6 +16,8 @@ class DataFragment : Fragment() {// se hereda de clase Fragment
 
     private lateinit var txtInfo :EditText
     private lateinit var btnEnviar :Button
+    private  lateinit var callback: DataListener
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -25,11 +29,24 @@ class DataFragment : Fragment() {// se hereda de clase Fragment
 
         btnEnviar.setOnClickListener(object  :View.OnClickListener{
             override fun onClick(view: View?) {
-                TODO("Not yet implemented")
+
             }
 
         })
         return view
     }
 
+    interface  DataListener{
+        fun enviarinfo(info: String)
+    }
+
+    override  fun onAttach(context : Context){
+        super.onAttacH(context)
+        callback = try {
+            context as DataListener
+        }catch (ex : Exception){
+           throw  java.lang.ClassCastException(context.toString())
+                    + "se debe implementar la interfaz DataListener"
+        }
+    }
 }
